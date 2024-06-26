4116c8a5ea665606041fc84ead788964a044ea66
pallav <pallav.dev@outlook.com>
2024-06-26 18:55:50 +0530

    Update app.py

--- Domain-scraper/app.py
+++ Domain-scraper/app.py
@@ -57,5 +57,5 @@ def get_transaction():
 def home():
     return jsonify({'Sucess': 'Hello World'}), 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+    # app.run(debug=True)
